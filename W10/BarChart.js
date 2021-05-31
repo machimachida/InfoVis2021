2e52658e1c2c8103bbcf1ac81999b5f422b321f3
Your Name <you@example.com>
2021-05-31 13:23:59 +0900

    Add W12

--- W10/BarChart.js
+++ W10/BarChart.js
@@ -8,8 +8,7 @@ class BarChart {
       title: config.title || '',
       xlabel: config.xlabel || '',
       ylabel: config.ylabel || '',
-      barColor: config.barColor || "steelblue",
-      isDesc: false,
+      duration: config.duration || 1000
     };
     this.data = data;
     this.init();
@@ -19,58 +18,58 @@ class BarChart {
     let self = this;
 
     self.svg = d3.select(self.config.parent)
-      .attr('width', self.config.width)
-      .attr('height', self.config.height);
+        .attr('width', self.config.width)
+        .attr('height', self.config.height);
 
     self.chart = self.svg.append('g')
-      .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+        .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
 
     self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
     self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
     self.xscale = d3.scaleLinear()
-      .range([0, self.inner_width]);
+        .range([0, self.inner_width]);
 
     self.yscale = d3.scaleBand()
-      .range([0, self.inner_height])
-      .paddingInner(0.2)
-      .paddingOuter(0.1);
+        .range([0, self.inner_height])
+        .paddingInner(0.2)
+        .paddingOuter(0.1);
 
     self.xaxis = d3.axisBottom(self.xscale)
-      .ticks(5)
-      .tickSizeOuter(0);
+        .ticks(5)
+        .tickSizeOuter(0);
 
     self.yaxis = d3.axisLeft(self.yscale)
-      .tickSizeOuter(0);
+        .tickSizeOuter(0);
 
     self.xaxis_group = self.chart.append('g')
-      .attr('transform', `translate(0, ${self.inner_height})`);
+        .attr('transform', `translate(0, ${self.inner_height})`);
 
     self.yaxis_group = self.chart.append('g');
 
     const title_space = 10;
     self.svg.append('text')
-      .style('font-size', '20px')
-      .style('font-weight', 'bold')
-      .attr('text-anchor', 'middle')
-      .attr('x', self.config.width / 2)
-      .attr('y', self.config.margin.top - title_space)
-      .text( self.config.title );
+        .style('font-size', '20px')
+        .style('font-weight', 'bold')
+        .attr('text-anchor', 'middle')
+        .attr('x', self.config.width / 2)
+        .attr('y', self.config.margin.top - title_space)
+        .text( self.config.title );
 
     const xlabel_space = 40;
     self.svg.append('text')
-      .attr('x', self.config.width / 2)
-      .attr('y', self.inner_height + self.config.margin.top + xlabel_space)
-      .text( self.config.xlabel );
+        .attr('x', self.config.width / 2)
+        .attr('y', self.inner_height + self.config.margin.top + xlabel_space)
+        .text( self.config.xlabel );
 
     const ylabel_space = 90;
     self.svg.append('text')
-      .attr('transform', `rotate(-90)`)
-      .attr('y', self.config.margin.left - ylabel_space)
-      .attr('x', -(self.config.height / 2))
-      .attr('text-anchor', 'middle')
-      .attr('dy', '1em')
-      .text( self.config.ylabel );
+        .attr('transform', `rotate(-90)`)
+        .attr('y', self.config.margin.left - ylabel_space)
+        .attr('x', -(self.config.height / 2))
+        .attr('text-anchor', 'middle')
+        .attr('dy', '1em')
+        .text( self.config.ylabel );
   }
 
   update() {
@@ -90,36 +89,37 @@ class BarChart {
   render() {
     let self = this;
 
+    const bar_color = 'steelblue';
     self.chart.selectAll("rect")
         .data(self.data)
-        .enter()
-        .append("rect")
+        .join("rect")
+        .transition().duration(self.config.duration)
         .attr("x", 0)
         .attr("y", d => self.yscale(d.label))
         .attr("width", d => self.xscale(d.value))
         .attr("height", self.yscale.bandwidth())
-        .attr("fill", self.config.barColor);
+        .attr("fill", bar_color);
 
     self.xaxis_group.call(self.xaxis);
-    self.yaxis_group.call(self.yaxis);
+
+    self.yaxis_group
+        .transition().duration(self.config.duration)
+        .call(self.yaxis);
   }
 
-  reverse() {
+  sort( order ) {
     let self = this;
 
-    this.data = self.data.reverse();
-    self.render();
-  }
-
-  sort() {
-    let self = this;
-
-    if(self.config.isDesc) {
-      this.data = self.data.sort((a, b) => { return -b.value+a.value; });
-    } else {
-      this.data = self.data.sort((a, b) => { return b.value-a.value; });
+    switch (order) {
+    case 'reverse':
+        self.data.reverse();
+        break;
+    case 'descend':
+        self.data.sort( (a,b) => b.value - a.value );
+        break;
+    case 'ascend':
+        self.data.sort( (a,b) => a.value - b.value );
+        break;
     }
-    this.config.isDesc = !self.config.isDesc
-    self.render();
   }
 }
